916b7c1dc71fcaaf6fd79e4e014350caf14eb81d
MuhammadMouaz <bc190406219@vu.edu.pk>
2024-08-10 00:36:33 +0500

    Initial commit with all terraform and Jenkinsfile

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami" {
+  default = "ami-04a81a99f5ec58529"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "key-pair for ngnix"
+}
+
+variable "vpc_id" {
+  default = "vpc-04c7c9f3da80795d3" # Replace with your default VPC ID if necessary
+}
+
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
